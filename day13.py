a6296432d073351d6c152c681d96a5cae9b65b7d
rslabon <rslabon@gmail.com>
2025-11-11 16:17:53 +0100

    day 13 - done

---
+++ day13.py
@@ -0,0 +1,65 @@
+from day5 import execute
+
+with open("./resources/day13.txt") as f:
+    program = [int(c) for c in f.read().strip().split(",")]
+
+def play():
+    data = program[:]
+    data[0] = 2
+    buffer = []
+    grid = {}
+    score = 0
+    ball = None
+    paddle = None
+
+    def get_input():
+        if ball and paddle:
+            if paddle[0] < ball[0]:
+                return 1
+            if paddle[0] > ball[0]:
+                return -1
+        return 0
+
+    def handle_output(v):
+        nonlocal ball, paddle, buffer, score, grid
+        buffer.append(v)
+        if len(buffer) == 3:
+            x = buffer[0]
+            y = buffer[1]
+            id = buffer[2]
+            if x == -1 and y == 0:
+                score = max(score, id)
+
+            grid[(x, y)] = id
+            buffer = []
+            if id == 3:
+                paddle = (x, y)
+            if id == 4:
+                ball = (x, y)
+
+    execute(data, get_input, handle_output)
+    return 2 not in grid.values(), score
+
+def part1():
+    output = []
+    execute(program, lambda: 1, lambda v: output.append(v))
+    result = 0
+    for i in range(0, len(output), 3):
+        id = output[i + 2]
+        if id == 2:
+            result += 1
+
+    assert result == 205
+
+
+def part2():
+    finished = False
+    score = 0
+    while not finished:
+        finished, score = play()
+
+    assert score == 10292
+
+
+part1()
+part2()
